335f19a261448f00a49a4798299deb3bf32d9b71
krandder <azsantos.k@gmail.com>
2026-07-12 02:55:25 -0300

    Prune obsolete FAO economics stack

--- src/interfaces/IFutarchyLiquidityManager.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IFutarchyLiquidityManager {
-    function initializeFromSale(uint256 faoAmount, bytes calldata spotAddData)
-        external
-        payable
-        returns (uint128 liquidityMinted);
-}
